ad7b123d8d4a1b48ed90dbf7695144afde3408c0
panjd123 <1747366367@qq.com>
2023-01-11 10:36:23 +0800

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,112 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "networkdata.h"
+#include <QPushButton>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    // 本地 IP，所有电脑都可以用这个 IP 指向自己
+    IP = "127.0.0.1";
+    // 端口，不要太简单，要避免和别的软件冲突
+    PORT = 16667;
+
+    this->ui->IPEdit->setText(IP);
+    this->ui->PORTEdit->setText(QString::number(PORT));
+
+    // 创建一个服务端
+    this->server = new NetworkServer(this);
+
+    lastOne = nullptr;
+
+    // 创建一个客户端
+    this->socket = new NetworkSocket(new QTcpSocket(),this);
+
+    connect(this->socket->base(),&QTcpSocket::connected,this,[&](){this->ui->connectLabel->setText("connection succeed");});
+    connect(this->socket,&NetworkSocket::receive,this,&MainWindow::receieveDataFromServer);
+    connect(this->server,&NetworkServer::receive,this,&MainWindow::receieveData);
+    connect(this->ui->clientSendButton,&QPushButton::clicked,this,&MainWindow::onClientSendButtonClicked);
+    connect(this->ui->serverSendButton,&QPushButton::clicked,this,&MainWindow::onServerSendButtonClicked);
+    connect(this->ui->showClientButton,&QPushButton::clicked,this,[&](){qDebug()<<clients;});
+    connect(this->ui->reSetButton,&QPushButton::clicked,this,&MainWindow::reSet);
+    connect(this->ui->reConnectButton,&QPushButton::clicked,this,&MainWindow::reConnect);
+    connect(this->ui->reStartButton,&QPushButton::clicked,this,&MainWindow::reStart);
+
+    // 客户端向 IP:PORT 连接，不会连到自己
+    this->socket->hello(IP,PORT);
+    // 阻塞等待，2000ms超时
+    this->socket->base()->waitForConnected(2000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::receieveData(QTcpSocket* client, NetworkData data)
+{
+    qDebug()<<"Server get a data: "<<client<<" "<<data.encode();
+    lastOne=client;
+    // 获得地址的字符串表示，调试用
+    QString ptrStr = QString("0x%1").arg((quintptr)client,
+                        QT_POINTER_SIZE, 16, QChar('0'));
+    this->ui->lastOneLabel->setText("LastOne: "+ptrStr);
+    this->clients.insert(client);
+    this->ui->serverGetEdit->setText(data.data1);
+}
+
+void MainWindow::receieveDataFromServer(NetworkData data)
+{
+    qDebug()<<"Client get a data: "<<data.encode();
+    this->ui->clientGetEdit->setText(data.data1);
+}
+
+void MainWindow::onClientSendButtonClicked()
+{
+    this->socket->send(NetworkData(OPCODE::CHAT_OP,this->ui->clientSendEdit->text(),"send by client"));
+}
+
+void MainWindow::onServerSendButtonClicked()
+{
+    if(lastOne)
+        this->server->send(lastOne,NetworkData(OPCODE::CHAT_OP,this->ui->serverSendEdit->text(),"send by server"));
+}
+
+void MainWindow::reStart()
+{
+    qDebug()<<"restart the server.";
+    this->ui->lastOneLabel->setText("LastOne: ");
+    this->ui->connectLabel->setText("disconnect");
+    disconnect(this->server,&NetworkServer::receive,this,&MainWindow::receieveData);
+    clients.clear();
+    delete this->server;
+    this->server = new NetworkServer(this);
+    // 端口相当于传信息的窗户，收的人要在这守着
+    this->server->listen(QHostAddress::Any,PORT);
+    lastOne = nullptr;
+    connect(this->server,&NetworkServer::receive,this,&MainWindow::receieveData);
+}
+
+void MainWindow::reConnect()
+{
+    qDebug()<<"client reconnect to the server.";
+    this->ui->connectLabel->setText("connection fail");
+
+    this->socket->bye();
+    this->socket->hello(IP,PORT);
+    if(!this->socket->base()->waitForConnected(3000)){
+        qDebug()<<"reconnect fail";
+    }
+}
+
+void MainWindow::reSet()
+{
+    this->ui->connectLabel->setText("connection fail");
+    IP=this->ui->IPEdit->text();
+    PORT=this->ui->PORTEdit->text().toInt();
+    this->reStart();
+    this->reConnect();
+}
---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSet>
+#include "networkserver.h"
+#include "networksocket.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    // 服务端
+    NetworkServer* server;
+    // 客户端
+    NetworkSocket* socket;
+    // 最后一个客户端
+    QTcpSocket* lastOne;
+    QString IP;
+    int PORT;
+    // 客户端池，NetworkServer 有一个 QList 的，但这里我想用 set，所以又弄了一个
+    QSet<QTcpSocket*> clients;
+
+
+private slots:
+    void receieveData(QTcpSocket* client, NetworkData data);
+    void receieveDataFromServer(NetworkData data);
+    void onClientSendButtonClicked();
+    void onServerSendButtonClicked();
+    void reStart();
+    void reConnect();
+    void reSet();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
